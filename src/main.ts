167320992508a5e0a444d1baf5eefd180b716ae7
Daniel Quiñonez <daniel.jose1999@outlook.com>
2026-04-06 15:34:38 -0500

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  override: true,
+});
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
